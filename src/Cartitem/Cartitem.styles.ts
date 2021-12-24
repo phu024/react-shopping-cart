4e1eb662d0d183859667410361e6d2946ce96a5e
Phuwadon Decahram <phuwadon.dev@gmail.com>
2021-12-25 01:30:32 +0700

    first commit

---
+++ src/Cartitem/Cartitem.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+font-family: "Arial", Helvetica, sans-serif;
+border: 1px solid lightblue;
+padding: 1rem;
+
+div {
+    flex: 1;
+}.information,.buttous {
+    display: flex;
+    justify-content: space-between;
+}
+
+img {
+    max-width: 80px;
+    object-fit: cover;
+    margin-left:40px;
+
+}
+`
